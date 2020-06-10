1d7a407842fc6c40fe14f4558f8363666b0493b3
Judali <jschwuchow@googlemail.com>
2020-06-10 14:15:06 +0200

    added repaint loop, fixed rows & columns

--- src/app/components/Cellboard.tsx
+++ src/app/components/Cellboard.tsx
@@ -20,8 +20,8 @@ class Cellboard extends React.Component<{}, CellboardState> {
     constructor(props: {}) {
         super(props);
         this.state = {
-            width: 500,
-            height: 500,
+            width: 800,
+            height: 600,
             cells: [],
             cellSize: 25
         }
@@ -32,18 +32,22 @@ class Cellboard extends React.Component<{}, CellboardState> {
     }
 
     initializeCellState() {
-        let rows = this.state.width / this.state.cellSize;
-        let columns = this.state.height / this.state.cellSize;
+        let rows = this.state.height / this.state.cellSize;
+        let columns = this.state.width / this.state.cellSize;
         let startCells: CellType[][] = [];
 
         for (let i = 0; i < rows; i++) {
             startCells[i] = [];
             for (let j = 0; j < columns; j++) {
+                // let random = Math.round(Math.random());
+                let random = Math.random();
+                if (random > .1) random = 0
+                else {random = 1}
                 let cell: CellType = {
-                    x: this.state.cellSize*i,
-                    y: this.state.cellSize*j,
+                    y: this.state.cellSize*i,
+                    x: this.state.cellSize*j,
                     size: this.state.cellSize,
-                    curState: Math.round(Math.random()),
+                    curState: random,
                 };
                 startCells[i][j] = cell;
             }
@@ -60,12 +64,14 @@ class Cellboard extends React.Component<{}, CellboardState> {
         })
     }
 
-    generate = (e: React.MouseEvent<HTMLDivElement>) => {
+    generate = () => {
+        let rows = this.state.height/this.state.cellSize;
+        let columns = this.state.width/this.state.cellSize;
         let newCells: CellType[][] = [...this.state.cells];
         let neighborCount = 0;
         // TODO change border cells
-        for( let i = 1; i < (this.state.width/this.state.cellSize)-1; i++) {
-            for (let j = 1; j < (this.state.height/this.state.cellSize)-1; j++) {
+        for( let i = 1; i < rows-1; i++) {
+            for (let j = 1; j < columns-1; j++) {
                 // console.log(this.state.cells[i][j]);
                 neighborCount = this.countNeighbors(i, j);
 
@@ -74,28 +80,33 @@ class Cellboard extends React.Component<{}, CellboardState> {
         }
 
         this.setState({ cells: newCells });
+
+        window.requestAnimationFrame(this.generate);
     }
 
     countNeighbors(curX: number, curY: number) {
         let neighbors = 0;
-
+        // console.log('Neighbour');
         for (let k = -1; k <= 1; k++) {
             for (let m = -1; m <= 1; m++) {
-                // TODO take out middle el
-                let {curState} = this.state.cells[curX+k][curY+m];
-                neighbors += curState;
+                if (!(k === 0 && m === 0)) {
+                    // console.log(this.state.cells[curX+k][curY+m], k, m);
+                    // console.log(curX+k,curY+m, k, m);
+                    let {curState} = this.state.cells[curX+k][curY+m];
+                    neighbors += curState;
+                }
             }
         }
         return neighbors;
     }
 
     applyRulesOfLife(neighbors: number, newCells: CellType[][], curX: number, curY: number) {
-        let cell = newCells[curX][curY];
-        let cellState = cell.curState;
+        let newCell = newCells[curX][curY];
+        let cellState = this.state.cells[curX][curY].curState;
 
-        if (cellState === 1 && neighbors >= 4) cell.curState = 0; // Overpopulation (DEATH)
-        else if (cellState === 1 && neighbors <= 1) cell.curState = 0; // Lonliness (DEATH)
-        else if (cellState === 0 && neighbors === 3) cell.curState = 1; // BIRTH
+        if (cellState === 1 && neighbors >= 4) newCell.curState = 0; // Overpopulation (DEATH)
+        else if (cellState === 1 && neighbors <= 1) newCell.curState = 0; // Lonliness (DEATH)
+        else if (cellState === 0 && neighbors === 3) newCell.curState = 1; // BIRTH
         else {} // STASIS: stays alive / dead
     }
 
